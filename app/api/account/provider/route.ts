b8503e837bc780d91cff6baeb45600f489405b9a
ArtDz <dzugaev1998@bk.ru>
2025-02-25 20:47:57 +0300

    Implement Account by provider route

---
+++ app/api/account/provider/route.ts
@@ -0,0 +1,26 @@
+import handleError from "@/lib/handlers/error"
+import { APIErrorResponse } from "@/types/global"
+import { NotFoundError, ValidationError } from "@/lib/http-errors"
+import { AccountSchema } from "@/lib/validations"
+import { NextResponse } from "next/server"
+import Account from "@/database/account.model"
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json()
+  if (!providerAccountId) throw new NotFoundError("Email")
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    })
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors)
+
+    const account = await Account.findOne({ providerAccountId })
+    if (!account) throw new NotFoundError("Account")
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 })
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse
+  }
+}
